d2a867a147074bc0d2644e377dfea8047f8b7392
brookemckim <brooke.mckim@gmail.com>
2012-01-16 20:09:52 -0500

    Completion scripts for workspace

--- system/aliases.sh
+++ system/aliases.sh
@@ -1,3 +1,6 @@
 alias sourceme='source ~/.bash_profile'
 alias keyup='ssh-copy-id'
 alias cleardns='dscacheutil -flushcache'
+
+function c { cd $HOME/Workspace/$1;        }
+function t { cd $HOME/Workspace/telvue/$1; }
---
+++ system/completions.sh
@@ -0,0 +1,2 @@
+complete -C $HOME/.dotfiles/system/completion_scripts/workspace_completion -o default c
+complete -C $HOME/.dotfiles/system/completion_scripts/telvue_completion -o default t
