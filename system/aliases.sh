8eb0209ca04f9820ef0caad5a993b5a5f80bb8ed
brookemckim <brooke.mckim@gmail.com>
2012-02-29 13:08:31 -0500

    screen aliases

--- system/aliases.sh
+++ system/aliases.sh
@@ -1,6 +1,9 @@
 alias sourceme='source ~/.bash_profile'
 alias keyup='ssh-copy-id'
 alias cleardns='dscacheutil -flushcache'
+alias sc=screen
+alias sr='screen -r'
+
 
 function c { cd $HOME/Workspace/$1;        }
 function t { cd $HOME/Workspace/telvue/$1; }
