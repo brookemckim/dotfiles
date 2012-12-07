1e0d69e58af2e6cf4a30704b7fe93a875ae359ff
brookemckim <brooke.mckim@gmail.com>
2012-12-06 21:14:07 -0500

    Added aliases

--- system/aliases.sh
+++ system/aliases.sh
@@ -3,8 +3,11 @@ alias keyup='ssh-copy-id'
 alias cleardns='sudo killall -HUP mDNSResponder'
 alias sc=screen
 alias sr='screen -r'
-
+alias timestamp="date '+%Y%m%d%H%M%S'"
 
 function c { cd $HOME/Workspace/$1;        }
 function t { cd $HOME/Workspace/telvue/$1; }
 function d { cd $HOME/Desktop/$1;          }
+
+# Remove all .svn directories from pwd. 
+function killsvn { find . -name .svn -print0 | xargs -0 rm -rf; }
